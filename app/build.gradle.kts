838103472274f78ae343bcf36a544aabfb75aaab
SatoX69 <mdjsusbinsus@gmail.com>
2026-06-23 16:41:07 +0600

    5th

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,10 +14,12 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
     kotlinOptions {
         jvmTarget = "17"
     }
@@ -29,14 +31,13 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.activity:activity-ktx:1.8.2")
-    
+
     val cameraxVersion = "1.3.1"
     implementation("androidx.camera:camera-core:$cameraxVersion")
     implementation("androidx.camera:camera-camera2:$cameraxVersion")
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
-    
+
     implementation("androidx.lifecycle:lifecycle-service:2.6.2")
     implementation("androidx.documentfile:documentfile:1.0.1")
 }
-
